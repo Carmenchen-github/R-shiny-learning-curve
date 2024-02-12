3bec63c8d1bd2c02aed0e7748f88abc8b4bb29ec
Carmenchen-github <zhaojingchen.work@outlook.com>
2024-02-12 13:56:14 +0800

    first app about population census data

---
+++ Shiny/census_app/cesus_app.R
@@ -0,0 +1,64 @@
+
+
+
+
+#install.packages(c("maps", "mapproj"))
+
+library(maps)
+library(mapproj)
+source("helper.R")
+counties <- readRDS("counties.rds")
+percent_map(counties$asian, "darkgreen", "% Asian",min = 50,max = 100)
+
+
+# User interface ----
+ui <- fluidPage(
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with
+        information from the 2010 US Census."),
+      
+      selectInput("var",
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range",
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+    
+    mainPanel(plotOutput("map"))
+  )
+)
+
+# Server logic ----
+server <- function(input, output) {
+  output$map <- renderPlot({
+    data <- switch(input$var,
+                   "Percent White" = counties$white,
+                   "Percent Black" = counties$black,
+                   "Percent Hispanic" = counties$hispanic,
+                   "Percent Asian" = counties$asian)
+    color <- switch(input$var,
+                    "Percent White" = "darkgreen",
+                    "Percent Black" = "black",
+                    "Percent Hispanic" = "darkorange",
+                    "Percent Asian" = "darkviolet")
+    
+    legend <- switch(input$var,
+                     "Percent White" = "% White",
+                     "Percent Black" = "% Black",
+                     "Percent Hispanic" = "% Hispanic",
+                     "Percent Asian" = "% Asian")
+    
+    percent_map(data, color, legend, input$range[1], input$range[2])
+  })
+}
+
+
+# Run app ----
+shinyApp(ui, server)
